be7be64910ba77374c8476343540e84538cd9c28
jsgreen82 <jacquegreen@gmail.com>
2017-12-20 18:25:48 -0700

    initial files

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Leia",
+    "photo" : "https://geekreply.com/wp-content/uploads/2017/04/Princess-Leia.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Han",
+    "photo" : "http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2016/06/han-solo-star-wars-4.png?itok=4L0CLbKh",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Luke",
+    "photo" : "https://www.starwarsnewsnet.com/wp-content/uploads/2014/01/20130205225011-Luke_Skywalker11.png",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Lord Vader",
+    "photo" : "https://brightcove04pmdo-a.akamaihd.net/3653334524001/3653334524001_5381043941001_5379748916001-vs.jpg?pubId=3653334524001&videoId=5379748916001",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "3-CPO",
+    "photo" : "http://cdn.playbuzz.com/cdn/be824980-592b-4a89-8eb9-66a137c3ac79/fdc058fa-3432-4339-a06d-c616c783f829.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Admiral Ackbar",
+    "photo" : "https://lumiere-a.akamaihd.net/v1/images/admiralackbar1_2_845df144.jpeg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Obi Wan",
+    "photo" : "https://cdn.vox-cdn.com/thumbor/YP3HK6voEMeWojSG9aGdJQMYlYY=/0x0:1300x650/1200x800/filters:focal(537x203:745x411)/cdn.vox-cdn.com/uploads/chorus_image/image/56242795/Obi_Wan.0.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Emperor Palpatine",
+    "photo" : "http://115.imagebam.com/download/TBv9NzbkgvSy6cjhKpGFNA/49122/491211192/Palpatine.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Yoda",
+    "photo" : "https://vignette.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Finn",
+    "photo" : "http://vignette3.wikia.nocookie.net/starwars/images/3/38/FinnHS-Fathead.png/revision/latest?cb=20150908052707",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Rey",
+    "photo" : "http://freebeacon.com/wp-content/uploads/2016/01/star-wars-the-force-awakens-new-tv-spot-rey.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Kylo Ren",
+    "photo" : "https://static0.srcdn.com/wp-content/uploads/2017/10/Star-Wars-the-Last-Jedi-Kylo-Ren-Talks-to-Rey.jpg",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Chewbacca",
+    "photo" : "https://lph5i1b6c053kq7us26bdk75-wpengine.netdna-ssl.com/wp-content/uploads/2013/01/chewbacca.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Maz",
+    "photo" : "https://lumiere-a.akamaihd.net/v1/images/maz-bio-gallery-1_d900f764.jpeg?region=191%2C78%2C1089%2C544&width=768",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
